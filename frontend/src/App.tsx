afefc02d6a9a82533f3c135d42be564fe0c97bbc
Vadym Repetskyi <vadym.repetskyi@gmail.com>
2023-10-22 01:15:59 +0200

    More darkness!

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,10 @@ import { memo } from "react";
 
 export const App = memo(() => {
   return (
-    <div className="flex flex-col h-screen bg-neutral-800 p-4 gap-4">
-      <div className="text-neutral-100 font-bold flex items-center">
+    <div className="flex text-neutral-100 flex-col h-screen bg-neutral-800 p-4 gap-4">
+      <div className="flex items-center">
         <h1 className="text-2xl">Building plot search</h1>
-        <span className="h-8 mx-4 w-[2px] bg-neutral-100 rounded-full" />
+        <span className="h-6 mx-4 w-[1px] bg-neutral-100 rounded-full" />
         <div className="font-normal flex gap-4">
           <button>Guide</button>
           <button>Settings</button>
@@ -13,15 +13,15 @@ export const App = memo(() => {
         </div>
       </div>
       <div className="flex flex-1 gap-4">
-        <div className="flex flex-col w-[500px] gap-4">
-          <div className="flex-1 bg-neutral-100 rounded-md p-4">
+        <div className="flex flex-col w-[500px] max-w-[50%] gap-4">
+          <div className="flex-1 bg-neutral-700 rounded-md p-4">
             <h2>Filters</h2>
           </div>
-          <div className="flex-1 bg-neutral-100 rounded-md p-4">
+          <div className="flex-1 bg-neutral-700 rounded-md p-4">
             <h2>Results</h2>
           </div>
         </div>
-        <div className="flex-1 bg-neutral-100 rounded-md p-4">Map</div>
+        <div className="flex-1 bg-neutral-700 rounded-md p-4">Map</div>
       </div>
     </div>
   );
