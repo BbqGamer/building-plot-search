9e9e37a62ac5567133581b978df3d55271240b10
BbqGamer <korba.adam@gmail.com>
2023-10-26 22:25:32 +0200

    Add endpoint for returning all districts

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from typing import Union
 from app.plots import import_plot_data, process_plot_id_column
+from app.districts import all_districts, District
 import logging
 
 from fastapi import FastAPI
@@ -19,4 +20,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/districts/")
+def get_all_districts() -> list[District]:
+    return all_districts()
 
