4bb0cb6708599203756e50a714120bf8feee5789
Vadym Repetskyi <vadym.repetskyi@gmail.com>
2023-11-06 23:23:48 +0100

    Added CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,26 @@
+import logging
 from typing import Union
-from app.plots import (
-    import_plot_data,
-    process_plot_id_column,
-    plots_for_district,
-    prepare_plot_dataframe
-)
 
 from app.buildings import import_building_data
-from app.districts import all_districts, District
-import logging
-
+from app.districts import District, all_districts
+from app.plots import (
+    import_plot_data,
+    plots_for_district,
+    prepare_plot_dataframe,
+    process_plot_id_column,
+)
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 logging.basicConfig(level=logging.INFO)
 
@@ -22,13 +30,16 @@ process_plot_id_column(plots)
 
 buildings = import_building_data()
 
+
 @app.get("/")
 def read_root():
     return {"Building": "Plot Search"}
 
+
 @app.get("/districts/")
 def get_all_districts() -> list[District]:
     return all_districts()
+
 
 @app.get("/plots/")
 def get_plots_for_district(district_id: int, min_area: int, max_area: int):
