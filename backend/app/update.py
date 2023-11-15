6c4261c426715033b1a61b1bd6e0af29e56ec32b
Momentow <bogdan.konovalenko@gmail.com>
2023-11-15 10:42:17 +0100

    added up_to_date checker

--- backend/app/update.py
+++ backend/app/update.py
@@ -5,6 +5,7 @@ import pathlib
 import io
 import os
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 DATA_DIR = pathlib.Path('data')
 last_update = None
@@ -36,13 +37,20 @@ def is_data_up_to_date() -> bool:
     if response.status_code == 200:
         logging.info("Generating BeautifulSoup structure and querring for the field...")
         soup = BeautifulSoup(response.text, 'html.parser')
-        
         specific_field = soup.select_one(update_field_selector)
         logging.info("Querring completed.")
 
         if specific_field:
             logging.info(f"Last data version update: {specific_field.text.strip()}")
             last_data_version = specific_field.text
+            date_format = "%d.%m.%Y %H:%M"
+            if last_download == None:
+                logging.info("FIRST update, anyway will be executed.")
+                return False
+            logging.info(f"Last donwloaded version is ({last_download}), last available data from ({last_data_version}).")
+            if datetime.strftime(last_download, date_format) >= datetime.strftime(last_data_version, date_format):
+                last_download = last_data_version
+                return True
         else:
             logging.info(f"Field not found using selector: {update_field_selector}")
     else:
