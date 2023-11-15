6f351dac012acdac0f854b14d614e4efe665623f
Vadym Repetskyi <vadym.repetskyi@gmail.com>
2023-11-15 18:15:03 +0100

    Cleaned up autoupdate

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,4 @@
 import logging
-from app.plots import (
-    import_plot_data,
-    process_plot_id_column,
-    plots_for_district,
-    prepare_plot_dataframe
-)
 
 from app.buildings import import_building_data
 from app.districts import District, all_districts
@@ -14,16 +8,10 @@ from app.plots import (
     prepare_plot_dataframe,
     process_plot_id_column,
 )
-from fastapi import FastAPI, Depends
-from fastapi.middleware.cors import CORSMiddleware
-
+from app.update import delete_all_data, download_and_unzip_archive, is_data_up_to_date
 from apscheduler.schedulers.background import BackgroundScheduler
-from app.update import (
-    is_data_up_to_date, 
-    download_and_unzip_archive, 
-    delete_all_data
-)
-
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -37,24 +25,32 @@ app.add_middleware(
 
 logging.basicConfig(level=logging.INFO)
 
+
+@app.get("/update-data/")
+def update_data():
+    logging.info("Autoupdating the data...")
+    if not is_data_up_to_date():
+        logging.info("Data is not up to date.")
+        delete_all_data()
+        download_and_unzip_archive(
+            "https://bip.geopoz.poznan.pl/download/119/8781/data.zip", "data"
+        )
+        download_and_unzip_archive(
+            "https://bip.geopoz.poznan.pl/download/119/8782/data.zip", "data"
+        )
+
+
+update_data()
+
 scheduler = BackgroundScheduler()
 scheduler.start()
+scheduler.add_job(update_data, "cron", day="*")
 
 plots = import_plot_data()
 prepare_plot_dataframe(plots)
 process_plot_id_column(plots)
 
 buildings = import_building_data()
-
-def update_data_task():
-    logging.info("Autoupdating the data...")
-    if not is_data_up_to_date():
-        logging.info("Data is not up to date.")
-        delete_all_data()
-        download_and_unzip_archive("https://bip.geopoz.poznan.pl/download/119/8781/data.zip", "data")
-        download_and_unzip_archive("https://bip.geopoz.poznan.pl/download/119/8782/data.zip", "data")
-
-scheduler.add_job(update_data_task, 'cron', month='*', hour=0, minute=0)
 
 
 @app.get("/")
@@ -71,12 +67,7 @@ MAX_AREA = 1000000
 
 
 @app.get("/plots/")
-def get_plots_for_district(district_id: int = 0, min_area: float = 0.0, max_area: float = MAX_AREA):
+def get_plots_for_district(
+    district_id: int = 0, min_area: float = 0.0, max_area: float = MAX_AREA
+):
     return plots_for_district(plots, district_id, min_area, max_area)
-
-
-@app.get("/update_data/")
-def manual_data_update():
-    delete_all_data()
-    download_and_unzip_archive("https://bip.geopoz.poznan.pl/download/119/8781/data.zip", "data")
-    download_and_unzip_archive("https://bip.geopoz.poznan.pl/download/119/8782/data.zip", "data")
--- backend/app/update.py
+++ backend/app/update.py
@@ -1,16 +1,18 @@
-import logging
-import requests
-import zipfile
-import pathlib
 import io
+import logging
 import os
-from bs4 import BeautifulSoup
+import pathlib
+import zipfile
 from datetime import datetime
 
-DATA_DIR = pathlib.Path('data')
+import requests
+from bs4 import BeautifulSoup
+
+DATA_DIR = pathlib.Path("data")
 last_update = None
 
-def download_and_unzip_archive(zip_url:str, extract_to:str) -> None:
+
+def download_and_unzip_archive(zip_url: str, extract_to: str) -> None:
     """Download .zip archive from provided URL and unpack it to the target diractory."""
     logging.info("Downloading the .zip...")
     response = requests.get(zip_url)
@@ -22,13 +24,19 @@ def download_and_unzip_archive(zip_url:str, extract_to:str) -> None:
             zip_ref.extractall(extract_to)
         logging.info("Extraction completed.")
     else:
-        logging.info(f"Failed to download the file. Status code: {response.status_code}.")
+        logging.info(
+            f"Failed to download the file. Status code: {response.status_code}."
+        )
+
 
 updates_url = "https://bip.geopoz.poznan.pl/gpb/rejestr/1675,dok.html"
 update_field_selector = "#table-listing table tr:nth-child(2) td:first-child"
 last_download = None
 
+
 def is_data_up_to_date() -> bool:
+    global last_download
+
     """Returns True if there are no updates or False if data not up to date."""
     logging.info(f"Request to {updates_url} ...")
     response = requests.get(updates_url)
@@ -36,7 +44,7 @@ def is_data_up_to_date() -> bool:
 
     if response.status_code == 200:
         logging.info("Generating BeautifulSoup structure and querring for the field...")
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.text, "html.parser")
         specific_field = soup.select_one(update_field_selector)
         logging.info("Querring completed.")
 
@@ -47,18 +55,25 @@ def is_data_up_to_date() -> bool:
             if last_download == None:
                 logging.info("FIRST update, anyway will be executed.")
                 return False
-            logging.info(f"Last downloaded version is ({last_download}), last available data from ({last_data_version}).")
-            if datetime.strftime(last_download, date_format) >= datetime.strftime(last_data_version, date_format):
+            logging.info(
+                f"Last downloaded version is ({last_download}), last available data from ({last_data_version})."
+            )
+            if datetime.strftime(last_download, date_format) >= datetime.strftime(
+                last_data_version, date_format
+            ):
                 last_download = last_data_version
                 return True
         else:
             logging.info(f"Field not found using selector: {update_field_selector}")
     else:
-        logging.info(f"Failed to download the file. Status code: {response.status_code}.")
-    
+        logging.info(
+            f"Failed to download the file. Status code: {response.status_code}."
+        )
+
     return False
 
-def delete_all_data(directory_path = "data") -> None:
+
+def delete_all_data(directory_path="data") -> None:
     """Made just for deleting old data from 'data' directory"""
     logging.info("Deleting of old data is started.")
     try:
