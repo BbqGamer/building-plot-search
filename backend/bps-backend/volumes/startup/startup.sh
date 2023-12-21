79b9c2988a8985631a929489c23ddba3706fde92
Vadym Repetskyi <vadym.repetskyi@gmail.com>
2023-12-21 19:43:21 +0100

    Adjusted the backend deployment configuration for the University VM

--- backend/bps-backend/volumes/startup/startup.sh
+++ backend/bps-backend/volumes/startup/startup.sh
@@ -1,18 +1,16 @@
-echo Waiting a minute for the DNS record to be created...
-sleep 1m
-
 cp -a /secrets/. /etc/letsencrypt/
-echo Copied the secrets from the volume
+echo The secrets have been imported
 
 (
     sleep 5m
     while :; do
         chmod -R 777 /etc/letsencrypt/ /secrets/
         cp -r -L /etc/letsencrypt/. /secrets/
-        echo Copied the secrets to the volume
+	echo The secrets have been exported
         sleep 8d
     done
 ) &
 
 echo Starting the nginx-certbot service
 /scripts/start_nginx_certbot.sh
+
